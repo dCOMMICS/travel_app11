310ca32ff5da9fef6fd283395af0004e3cdd90b7
dCOMMICS <sanwurld@gmail.com>
2023-10-15 00:55:48 +0300

    ✌

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,13 +13,13 @@ const NavBar = () => {
     <ul className="hidden h-full gap-12 lg:flex">
 
     {NAV_LINKS.map((link) =>(
-        <Link href={link.href} key={link.key}>
+        <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all haver:font-bold">
             {link.label}
         </Link>
     ))}
 
     </ul>
-</Link>
+</Link> 
 
     </nav>
   )
