16b4e51b5f91b8a751e9f1544a091e55a99ef9ee
dCOMMICS <sanwurld@gmail.com>
2023-10-15 00:04:46 +0300

    🚀💪🏿

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="border-2 border-red-600 flexBetween max-container padding-container relative z-30 py-5" ></nav>
   )
 }
 
