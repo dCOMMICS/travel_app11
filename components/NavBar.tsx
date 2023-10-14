d0b6f6b03f6dd7e7f4a6ff34642946434aca06fc
dCOMMICS <sanwurld@gmail.com>
2023-10-15 00:06:43 +0300

    💪🏿

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link"
 
 
 const NavBar = () => {
   return (
-    <nav className="border-2 border-red-600 flexBetween max-container padding-container relative z-30 py-5" ></nav>
+    <nav className="border-2 border-red-600 flexBetween max-container padding-container relative z-30 py-5" >
+
+<Link href="/"></Link>
+
+    </nav>
   )
 }
 
