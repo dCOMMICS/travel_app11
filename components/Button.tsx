cc7391e6c2394da6bbc346b1f303987f8dc95ce5
dCOMMICS <sanwurld@gmail.com>
2023-10-17 14:30:06 +0300

    🚀💪🏿

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type ButtonProps = {
@@ -12,6 +13,8 @@ type ButtonProps = {
 const button = ({type,title,icon,variant}:ButtonProps) => {
   return (
     <button type={type} >
+
+      {icon &&<Image src={icon} alt={title} width={24} height={24} height={24}}
        Login 
     </button>
   )
