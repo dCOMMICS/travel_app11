1bb7d07de77ef326cb847aa2ecfc0e3048bd18cc
dCOMMICS <sanwurld@gmail.com>
2023-10-17 14:43:40 +0300

    💪🏿🚀

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,7 +14,7 @@ const button = ({type,title,icon,variant}:ButtonProps) => {
   return (
     <button type={type} >
 
-      {icon &&<Image src={icon} alt={title} width={24} height={24} height={24}}
+      {icon &&<Image src={icon} alt={title} width={24} height={24}/>}
        Login 
     </button>
   )
