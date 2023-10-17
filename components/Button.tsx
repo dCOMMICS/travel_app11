69035e2dea7e5edf8242252fe3eaa16f237c09e9
dCOMMICS <sanwurld@gmail.com>
2023-10-17 15:26:31 +0300

    🚀💪🏿

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,9 @@ type ButtonProps = {
 
 const button = ({type,title,icon,variant}:ButtonProps) => {
   return (
-    <button type={type} >
+    <button
+    className={'flexCenter gap-3 rounded-full border'}
+     type={type} >
 
       {icon &&<Image src={icon} alt={title} width={24} height={24}/>}
        <label className="bold-16 whitespace-nowrap">{title}</label>
